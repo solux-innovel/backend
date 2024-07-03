d0fd6b0bbf4e9b25a49c9e665c0e50a63bf7741e
making-a-scene <4936joo@naver.com>
2024-07-03 11:23:31 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/solux/innovel/InnovelApplication.java
@@ -0,0 +1,13 @@
+package com.solux.innovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solux/innovel/InnovelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solux.innovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
