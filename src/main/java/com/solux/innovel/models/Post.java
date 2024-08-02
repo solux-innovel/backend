02376cc367a27bf967b3c4208bcf8ab8e34b0ebe
Answl <118355919+Answl@users.noreply.github.com>
2024-08-03 05:30:16 +0900

    로그인 기능 수정
    이미 존재하는 사용자 기능 수정

--- src/main/java/com/solux/innovel/models/Post.java
+++ src/main/java/com/solux/innovel/models/Post.java
@@ -25,6 +25,8 @@ public class Post extends BaseEntity {
 
     private String title;
 
+    @Lob
+    @Column(columnDefinition = "LONGTEXT")
     private String content;
 
     private String summary;
