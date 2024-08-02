cadafe99a483f4bde3d38d9521b79b4785259562
Answl <118355919+Answl@users.noreply.github.com>
2024-08-03 03:02:28 +0900

    URL 수정 및 충돌코드 제거

--- src/main/java/com/solux/innovel/post/postSave/PDBCheckController.java
+++ src/main/java/com/solux/innovel/post/postSave/PDBCheckController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/posts")
+@RequestMapping("/innovel/posts")
 public class PDBCheckController {
     private final PostRepository postRepository;
 
